8b7b3fe78c75717a82aed98dc9148b327a998712
Nicolas <funestunes@hotmail.com>
2018-11-11 01:15:25 -0300

    Agregue una clase mapa con 2 enteros para las posiciones solamente para hacer mi primer branch request

---
+++ src/fiuba/algo3/algoempires/Mapa.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.algoempires;
+
+public class Mapa {
+    private int LargoHorizontal;
+    private int LargoVertical;
+
+public Mapa(int DimensionHorizontal,int DimensionVertical){
+    this.LargoHorizontal=DimensionHorizontal;
+    this.LargoVertical=DimensionVertical;
+}
+
+
+}
