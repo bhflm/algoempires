7466d269fabf9b1a10ef80cd33c27453eba6ba5c
bhflm <germanbonin@gmail.com>
2018-11-11 13:17:32 -0300

    add dos tipos de edificios muy raw

---
+++ src/fiuba/algo3/algoempires/Edificio.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.algoempires;
+
+public abstract class Edificio {
+    protected int vida;
+    protected int costo;
+
+
+    public int getVida(){ return this.vida; }
+    public int getCosto(){
+        return this.costo;
+    }
+
+
+}
---
+++ src/fiuba/algo3/algoempires/PlazaCentral.java
@@ -0,0 +1,12 @@
+package fiuba.algo3.algoempires;
+
+public class PlazaCentral extends Edificio {
+
+    public PlazaCentral(){
+        vida = 450;
+        costo = 1000;
+        //TurnosRestantes = ... ??;
+    }
+
+    //Metodo tipo estado en pikachu pero que se vaya restando turnos.
+}
