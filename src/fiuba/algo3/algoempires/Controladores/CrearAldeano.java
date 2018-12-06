45aa7be51a9c31c6f7aa9d1605837e31135c46bf
Nicolas <funestunes@hotmail.com>
2018-12-06 16:33:05 -0300

    Cambio las acciones usando los metodos de felipe

--- src/fiuba/algo3/algoempires/Controladores/CrearAldeano.java
+++ src/fiuba/algo3/algoempires/Controladores/CrearAldeano.java
@@ -1,19 +1,27 @@
 package fiuba.algo3.algoempires.Controladores;
 
+import fiuba.algo3.algoempires.Entidades.PlazaCentral;
+import fiuba.algo3.algoempires.Juego;
+import fiuba.algo3.algoempires.Jugador;
 import fiuba.algo3.algoempires.Vistas.VistaPrincipal;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class CrearAldeano implements EventHandler<ActionEvent> {
-    VistaPrincipal vistaPrincipal;
+    VistaPrincipal vistaTableroJuegoActual;
 
     public CrearAldeano(VistaPrincipal vistaPrincipal) {
-        this.vistaPrincipal=vistaPrincipal;
+        this.vistaTableroJuegoActual=vistaPrincipal;
         }
 
     @Override
     public void handle(ActionEvent actionEvent){
-        vistaPrincipal.nombreAccion="CrearAldeano";
-        vistaPrincipal.ofrecerCasilleros(vistaPrincipal.elJuegoEs().getmapa());
+        Juego elJuego=vistaTableroJuegoActual.elJuegoEs();
+        Jugador jugador=elJuego.getActual();
+        jugador.crearAldeano(elJuego, (PlazaCentral) vistaTableroJuegoActual.getCasilleroSeleccionado().getUbicable(), elJuego.getmapa());
+        vistaTableroJuegoActual.cambiarJugadorEnTurno(elJuego);
+        vistaTableroJuegoActual.actualizarTableroV2(elJuego.getmapa());
+        vistaTableroJuegoActual.borrarSetAcciones();
+        vistaTableroJuegoActual.setAcciones();
     }
 }
--- src/fiuba/algo3/algoempires/Controladores/CrearEspadachin.java
+++ src/fiuba/algo3/algoempires/Controladores/CrearEspadachin.java
@@ -1,18 +1,27 @@
 package fiuba.algo3.algoempires.Controladores;
 
+import fiuba.algo3.algoempires.Entidades.Cuartel;
+import fiuba.algo3.algoempires.Entidades.PlazaCentral;
+import fiuba.algo3.algoempires.Juego;
+import fiuba.algo3.algoempires.Jugador;
 import fiuba.algo3.algoempires.Vistas.VistaPrincipal;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class CrearEspadachin implements EventHandler<ActionEvent> {
-    VistaPrincipal vistaPrincipal;
+    VistaPrincipal vistaTableroJuegoActual;
     public CrearEspadachin(VistaPrincipal vistaPrincipal) {
-        this.vistaPrincipal=vistaPrincipal;
+        this.vistaTableroJuegoActual=vistaPrincipal;
     }
 
     public void handle(ActionEvent actionEvent){
-        vistaPrincipal.nombreAccion="CrearEspadachin";
-        vistaPrincipal.ofrecerCasilleros(vistaPrincipal.elJuegoEs().getmapa());
+        Juego elJuego=vistaTableroJuegoActual.elJuegoEs();
+        Jugador jugador=elJuego.getActual();
+        jugador.crearEspadachin(elJuego, (Cuartel) vistaTableroJuegoActual.getCasilleroSeleccionado().getUbicable(), elJuego.getmapa());
+        vistaTableroJuegoActual.cambiarJugadorEnTurno(elJuego);
+        vistaTableroJuegoActual.actualizarTableroV2(elJuego.getmapa());
+        vistaTableroJuegoActual.borrarSetAcciones();
+        vistaTableroJuegoActual.setAcciones();
     }
 
 }
